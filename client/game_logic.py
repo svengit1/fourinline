0530ae60a22219b0831159c7f909cf868560a1fa
dorian <matic.dorian@gmail.com>
2019-04-21 17:14:56 +0200

    updated logic and implemented graphics

--- client/game_logic.py
+++ client/game_logic.py
@@ -1,15 +1,10 @@
-active_player = "blue"
 board = [0] * 42
 
 
-def turn():
-    global active_player
-
-    if active_player == "blue":
-        active_player = "red"
-        return 'red'
-    if active_player == "red":
-        active_player = "blue"
-        return 'blue'
+def turn(player):
+    if player == "blue":
+        return "red"
+    elif player == "red":
+        return "blue"
 
 
