d4d88e2c54b951ad182c7bdd13cd758aa531725f
Viktor Matic <viktor@inge-mark.com>
2019-04-25 18:51:02 +0200

    Fixtures and first test

--- client/game_logic.py
+++ client/game_logic.py
@@ -12,12 +12,23 @@ def turn(player):
 
 def check_game_state(state, row_length):
     shp = state.shape
-
+    result = []
+    print("Shape ", shp)
     for y in range(0, shp[0]):
+        inrow = 0
+        row = []
         for x in range(0, shp[1]):
-            print(x, y)
+            if state[y, x] == 1:
+                inrow += 1
+                row.append([y, x])
+            else:
+                inrow == 0
+                row = []
+            if inrow >= row_length:
+                result.append(row)
 
-    return np.array([[2,1], [2, 2], [2, 3]])
+    print(result)
+    return result
 
 
 
--- client/tests/test_game_logic.py
+++ client/tests/test_game_logic.py
@@ -20,7 +20,7 @@ class TestLogic(unittest.TestCase):
 
     def test_check_game_state_dim(self):
         np.testing.assert_array_equal(logic.check_game_state(fix.state_2_3, 3),
-                                      np.array([[2,1], [2, 2], [2, 3]]))
+                                      fix.result_2_3)
 
 
 if __name__ == '__main__':
