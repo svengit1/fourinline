3e65ad886450dc076dc9a100b6ba6ac49b9997a6
Viktor Matic <viktor@inge-mark.com>
2019-04-26 07:31:43 +0200

    Test to detect same tokens in a column

--- client/tests/test_game_logic.py
+++ client/tests/test_game_logic.py
@@ -18,9 +18,14 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(logic.turn('red'), 'blue')
         self.assertEqual(logic.turn('blue'), 'red')
 
-    def test_check_game_state_dim(self):
-        np.testing.assert_array_equal(logic.check_game_state(fix.state_2_3, 3),
-                                      fix.result_2_3)
+    def test_check_detect_same_in_rows(self):
+        np.testing.assert_array_equal(logic.check_game_state(fix.state_1, 3, 1),
+                                      fix.result_1)
+
+    def test_check_detect_same_in_columns(self):
+        np.testing.assert_array_equal(logic.check_game_state(fix.state_2, 3, 1),
+                                      fix.result_2)
+
 
 
 if __name__ == '__main__':
